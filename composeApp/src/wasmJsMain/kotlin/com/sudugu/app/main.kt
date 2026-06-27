f599f1fda2ce4b9f9aa859d5d8ba04144ede9908
MultiDev <2815968613@qq.com>
2026-06-27 08:35:00 +0800

    fix(wasm): use CanvasBasedWindow directly + kotlin.js.Date for time

--- composeApp/src/wasmJsMain/kotlin/com/sudugu/app/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/sudugu/app/main.kt
@@ -2,20 +2,11 @@ package com.sudugu.app
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
-import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    onWasmReady {
-        val body = kotlinx.browser.document.body!!
-        val canvas = kotlinx.browser.document.createElement("canvas")
-        body.appendChild(canvas)
-        // Fallback to CanvasBasedWindow for KMP web
-        CanvasBasedWindow(
-            canvasElementId = "sudugu-canvas",
-        ) {
-            val vm = rememberViewModel()
-            SuduguApp(vm)
-        }
+    CanvasBasedWindow("Sudugu") {
+        val vm = rememberViewModel()
+        SuduguApp(vm)
     }
 }
